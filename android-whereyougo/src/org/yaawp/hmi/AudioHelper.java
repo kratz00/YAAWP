2ace32c543867759347b5ea82e9d8b5f5a927f2c
CachingFoX <CachingFoX@mail-hauser.de>
2013-09-10 10:24:49 +0200

    added some log message for bad cases

--- android-whereyougo/src/org/yaawp/hmi/AudioHelper.java
+++ android-whereyougo/src/org/yaawp/hmi/AudioHelper.java
@@ -9,18 +9,23 @@ public class AudioHelper {
 	
 	private static final String TAG = "AudioHelper";
 	
-	public void playSound(byte[] data, String mime) {
+	public static void playSound(byte[] data, String mime) {
 		Logger.i(TAG, "playSound(" + (data != null ? data.length : 0) + ", "+ mime + ")");
 		
-		if (data == null || data.length == 0 || mime == null)
+		if (data == null || data.length == 0 || mime == null) {
+			Logger.e( TAG, "playSound(): invalid parameters" );
 			return;
+		}
+		
 		try {
 			ByteArrayInputStream bis = new ByteArrayInputStream(data);
 			if ("audio/x-wav".equals(mime)) {
 				A.getManagerAudio().playMp3File("audio", ".wav", bis);
 			} else if ("audio/mpeg".equals(mime)) {
 				A.getManagerAudio().playMp3File("audio", ".mp3", bis);
-			} 
+			} else {
+				Logger.e( TAG, "playSound(): unsupported mime-type" );
+			}
 		} catch (Exception e) {
 			Logger.e(TAG, "playSound() failed", e);
 		}
