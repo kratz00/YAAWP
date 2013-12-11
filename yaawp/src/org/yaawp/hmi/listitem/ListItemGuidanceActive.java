2c23c9f1f4602593ebec9c4c408168b56d6c911b
CachingFoX <cachingfox@mail-hauser.de>
2013-12-12 00:41:03 +0100

    [improvement] insert class ListItemGuidaneActive

---
+++ yaawp/src/org/yaawp/hmi/listitem/ListItemGuidanceActive.java
@@ -0,0 +1,68 @@
+package org.yaawp.hmi.listitem;
+
+import android.content.Context;
+import org.yaawp.R;
+import org.yaawp.guidance.interfaces.Guide;
+import org.yaawp.guidance.interfaces.GuidingListener;
+import org.yaawp.hmi.activities.GuidingActivity;
+import org.yaawp.hmi.helper.I18N;
+import org.yaawp.hmi.listitem.ListItem3ButtonsHint;
+import org.yaawp.hmi.panelbar.buttons.PanelBarButton;
+import org.yaawp.hmi.panelbar.buttons.PanelBarButtonStopGuidance;
+import org.yaawp.utils.A;
+
+public class ListItemGuidanceActive extends ListItem3ButtonsHint implements GuidingListener {
+
+	public Context mContext = null;
+	
+	public ListItemGuidanceActive( Context context ) {
+		super(	/* TODO I18N */ "Guidance active",
+				/* TODO I18N */ "Guidance to zone <i>" + A.getGuidingContent().getName() +"</i>" );
+		
+		mContext = context;
+		
+		AddButton( new PanelBarButton( I18N.get(R.string.navigate ), 
+				new PanelBarButton.OnClickListener() {
+					@Override
+					public boolean onClick() {									
+						GuidingActivity.callGudingScreen( mContext, GuidingActivity.CONTINUE_GUIDANCE_AT_EXIT );
+						return true;
+					}
+				}
+			)); 
+		
+		AddButton( new PanelBarButtonStopGuidance() );  
+		
+		enableCancelButton( false );
+		
+	    A.getGuidingContent().addGuidingListener(this);
+	   	
+	    // TODO remove listener 
+	    // A.getGuidingContent().removeGuidingListener(this);	
+	}
+	
+	@Override
+	public boolean isValid() {
+		return A.getGuidingContent().isGuiding();
+	}
+	
+	/* --- GuidanceListener ------------------------------------------------ */
+	@Override
+	public void receiveGuideEvent(Guide guide, String targetName, float azimuthToTarget, double distanceToTarget) {
+		
+	}
+	
+	@Override
+	public void trackGuideCallRecalculate() {
+	}	
+	
+	@Override
+	public void guideStart() {
+		notifyDataSetChanged();
+	}
+
+	@Override
+	public void guideStop() {
+		notifyDataSetChanged();
+	}	
+}
