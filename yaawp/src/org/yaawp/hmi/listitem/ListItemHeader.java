99b53860c73b517ab5addcde71a4a5b59cae92ef
CachingFoX <cachingfox@mail-hauser.de>
2013-12-14 00:03:05 +0100

    [refactoring] remove layout changes, simplify code

--- yaawp/src/org/yaawp/hmi/listitem/ListItemHeader.java
+++ yaawp/src/org/yaawp/hmi/listitem/ListItemHeader.java
@@ -1,54 +1,43 @@
 package org.yaawp.hmi.listitem;
 
-
 import org.yaawp.R;
 import org.yaawp.utils.Logger;
-import org.yaawp.utils.Utils;
-
 import android.content.Context;
-import android.graphics.Color;
 import android.text.Html;
 import android.view.View;
 import android.widget.TextView;
 
 public class ListItemHeader extends AbstractListItem {
 
-	private static String TAG = "CartridgeListAdapterItemHeader";
+	private static String TAG = "ListItemHeader";
     
-    private static final int PADDING = (int) Utils.getDpPixels(2.0f);
-    
-    private String mTitle;
+    private String mTitleLeft;
     private String mTitleRight;
     
-    public ListItemHeader( String title, String right ) {
+    public ListItemHeader( String titleLeft, String titleRight ) {
     	super( R.layout.list_separator );
-    	mTitle = title;
-    	mTitleRight = right;
+    	mTitleLeft = titleLeft;
+    	mTitleRight = titleRight;
     }    
     
+    @Override
 	public boolean isEnabled() {
 		return false;
 	}    
     
+	@Override
 	public void layout( Context context, View view  ) {
 	
-		try {
-						
-			view.setBackgroundColor(Color.LTGRAY);
-				
-			TextView tv = (TextView) view.findViewById(R.id.linearLayoutSeparatorHeadline);
-			tv.setBackgroundColor(Color.TRANSPARENT);
-			tv.setTextColor(Color.BLACK);
-			tv.setVisibility(View.VISIBLE);
-			tv.setText(Html.fromHtml(mTitle));
+		try {				
+		
+			TextView tvLeft  = (TextView) view.findViewById(R.id.linearLayoutSeparatorHeadline);
+			tvLeft.setVisibility(View.VISIBLE);
+			tvLeft.setText(Html.fromHtml(mTitleLeft));
 			
-			TextView tvR = (TextView) view.findViewById(R.id.linearLayoutSeparatorHeadline2);
-			tvR.setBackgroundColor(Color.TRANSPARENT);
-			tvR.setTextColor(Color.BLACK);
-			tvR.setVisibility(View.VISIBLE);
-			tvR.setText(Html.fromHtml(mTitleRight));
-		
-		
+			TextView tvRight = (TextView) view.findViewById(R.id.linearLayoutSeparatorHeadline2);
+			tvRight.setVisibility(View.VISIBLE);
+			tvRight.setText(Html.fromHtml(mTitleRight));
+
 		} catch (Exception e) {
 			Logger.e(TAG, "getView(" + view + ")", e);
 		}
