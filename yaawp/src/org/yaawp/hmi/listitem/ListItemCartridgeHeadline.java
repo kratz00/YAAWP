e882af5b0c67748b7357a0686e9e3aef3a61a96e
CachingFoX <cachingfox@mail-hauser.de>
2013-12-20 09:28:00 +0100

    [improvement] added author and version info

--- yaawp/src/org/yaawp/hmi/listitem/ListItemCartridgeHeadline.java
+++ yaawp/src/org/yaawp/hmi/listitem/ListItemCartridgeHeadline.java
@@ -1,5 +1,6 @@
 package org.yaawp.hmi.listitem;
 
+import org.yaawp.hmi.helper.I18N;
 import org.yaawp.hmi.listitem.ListItem3ButtonsHint;
 import org.yaawp.utils.CartridgeHelper;
 import org.yaawp.utils.Images;
@@ -7,6 +8,8 @@ import org.yaawp.utils.Images;
 import cz.matejcik.openwig.Engine;
 
 import org.yaawp.R;
+
+import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.view.View;
@@ -16,11 +19,13 @@ import org.yaawp.hmi.listitem.styles.*;
 public class ListItemCartridgeHeadline extends ListItem3ButtonsHint {
 
 	public ListItemCartridgeHeadline() {
-		super( false, null );
+		super( true, null );
 		    	
     	if ( Engine.instance != null && Engine.instance.cartridge != null ) {
     		mDataTextMajor = Engine.instance.cartridge.name; 
-    		mDataImageLeft = CartridgeHelper.getIconFrom( Engine.instance.cartridge, R.drawable.icon_gc_wherigo );
+    		mDataImageLeft = CartridgeHelper.getIconFrom( Engine.instance.cartridge, R.drawable.ic_launcher );
+    		mDataTextMinor = I18N.get(R.string.author) + " " + Engine.instance.gwcfile.getAuthor();
+    		mDataTextMinorRight = Engine.instance.gwcfile.getVersion();
     	} else {
     		mDataTextMajor = "{ERROR no engine or cartridge instance}"; 
     		mDataImageLeft = Images.getImageB(R.drawable.icon_gc_wherigo);
@@ -29,9 +34,13 @@ public class ListItemCartridgeHeadline extends ListItem3ButtonsHint {
     	// ----
     	mStyleBackground = new StyleBasics( View.VISIBLE, 0xFF24699c ); // wherigo-blue 
     	mStyleTextMajor  = new StyleText( View.VISIBLE, Color.TRANSPARENT, Color.WHITE, 18, Typeface.BOLD );
-    	mStyleTextMinor  = null;
+    	// mStyleTextMinor  = null;
     	mStyleImageLeft  = new StyleImage( View.VISIBLE, Color.TRANSPARENT, 48, 48, null );
     	mStyleCancelButton = null;		
 	}
+	
+	public void onListItemClicked( Activity activity ) {
+		return;
+	}		
 }
 
