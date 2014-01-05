ea2b5c6ad727b1ca88b28c6e2f27a56390103644
CachingFoX <cachingfox@mail-hauser.de>
2014-01-06 00:28:07 +0100

    [feature] add method distanceInMap()

--- yaawp/src/org/yaawp/maps/services/GenericOverlay.java
+++ yaawp/src/org/yaawp/maps/services/GenericOverlay.java
@@ -4,15 +4,27 @@ import org.yaawp.maps.services.MapProjection;
 import org.yaawp.positioning.Location;
 
 import android.graphics.Canvas;
+import android.graphics.Point;
 
 public abstract class GenericOverlay {
 
 	protected Location mLocation;
-	
+    private Point center = new Point();
+    private Point left = new Point();
+    
     public abstract void drawBitmap(Canvas canvas, MapProjection projection);   
     
     public void onLocationChanged( Location location ) {
     	mLocation = location;
     }
-           
+    
+    public int distanceInMap( float distanceMeter, MapProjection projection ) {
+        float longitudeLineDistance = mLocation.longitudeLineDistance();
+        double latitude = mLocation.getLatitude();
+        double longitude = mLocation.getLongitude();
+        projection.toPixels( latitude, longitude - (distanceMeter / longitudeLineDistance), left);
+        projection.toPixels( latitude, longitude, center);
+        return center.x - left.x;  
+    }
+  
 }
