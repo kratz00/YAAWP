6a4a3afaf875f54142de671e05d546be1e6f3fd8
CachingFoX <cachingfox@mail-hauser.de>
2014-01-06 01:30:48 +0100

    [feature] add zone boundary box, center, nearest point

--- yaawp/src/org/yaawp/maps/services/OverlayZones.java
+++ yaawp/src/org/yaawp/maps/services/OverlayZones.java
@@ -2,6 +2,9 @@ package org.yaawp.maps.services;
 
 import java.util.Vector;
 
+import org.yaawp.positioning.Location;
+import org.yaawp.utils.Logger;
+
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -12,14 +15,16 @@ import cz.matejcik.openwig.Zone;
 
 public class OverlayZones extends GenericOverlay {
 
-
+	private static String TAG = OverlayZones.class.getSimpleName();
+	
 	private Paint mPaintVisibleZoneFill = new Paint(Paint.ANTI_ALIAS_FLAG);
 	private Paint mPaintVisibleZoneBorder = new Paint(Paint.ANTI_ALIAS_FLAG);
 	// private Paint mPaintInvisibleZoneFill = new Paint(Paint.ANTI_ALIAS_FLAG);
 	// private Paint mPaintInvisibleZoneBorder = new Paint(Paint.ANTI_ALIAS_FLAG);
 	// private Paint mPaintBoundaryBoxZoneFill = null;
 	private Paint mPaintBoundaryBoxZoneBorder = new Paint(Paint.ANTI_ALIAS_FLAG);
-		
+	private Paint mPaintZoneCenterFill = new Paint(Paint.ANTI_ALIAS_FLAG);
+	private Paint mPaintZoneCenterBorder = new Paint(Paint.ANTI_ALIAS_FLAG);
 	
 	public OverlayZones() {
 		
@@ -31,39 +36,74 @@ public class OverlayZones extends GenericOverlay {
 		mPaintVisibleZoneBorder.setColor(Color.BLUE);
 		mPaintVisibleZoneBorder.setAlpha(196);
 		mPaintVisibleZoneBorder.setStrokeWidth(3);	
+	
+		mPaintZoneCenterFill.setStyle(Paint.Style.FILL);
+		mPaintZoneCenterFill.setColor(Color.RED);
+		mPaintZoneCenterFill.setAlpha(64);			
+
+		mPaintZoneCenterBorder.setStyle(Paint.Style.STROKE);
+		mPaintZoneCenterBorder.setColor(Color.BLUE);
+		mPaintZoneCenterBorder.setAlpha(196);
+		mPaintZoneCenterBorder.setStrokeWidth(2);		
+		
 		
 		// --- draw boundary box
 		mPaintBoundaryBoxZoneBorder.setStyle(Paint.Style.STROKE);
-		mPaintBoundaryBoxZoneBorder.setColor(Color.BLUE);
-		mPaintBoundaryBoxZoneBorder.setAlpha(128);
-		mPaintBoundaryBoxZoneBorder.setStrokeWidth(3);			
+		mPaintBoundaryBoxZoneBorder.setColor(Color.CYAN);
+		mPaintBoundaryBoxZoneBorder.setAlpha(196);
+		mPaintBoundaryBoxZoneBorder.setStrokeWidth(5);			
 	}
 	
-	public void drawZone( Zone z, Canvas canvas, MapProjection projection ) {
-		int number = z.points.length;
-		Point[] mPoints = new Point[number];
-		
-		for( int i=0; i<number; i++ ) {
-			mPoints[i] = new Point();
-			projection.toPixels( z.points[i].latitude, z.points[i].longitude, mPoints[i] );
-		}
-		
+	public void drawZone( Zone z, Canvas canvas, MapProjection projection ) {	
+		Point point = new Point();
 		Path wallpath = new Path();
+		Location centerLocation = new Location();
+		centerLocation.setLatitude( z.bbCenter.latitude); 
+		centerLocation.setLongitude( z.bbCenter.longitude );		
+		
+		// --- boundary box
+		wallpath.reset();
+		projection.toPixels( z.bbBottom, z.bbLeft, point );
+		wallpath.moveTo( point.x, point.y );
+		projection.toPixels( z.bbTop, z.bbLeft, point );
+		wallpath.lineTo( point.x, point.y );	
+		projection.toPixels(  z.bbTop, z.bbRight, point );
+		wallpath.lineTo( point.x, point.y );
+		projection.toPixels( z.bbBottom, z.bbRight, point );
+		wallpath.lineTo( point.x, point.y );	
+		wallpath.close();
+		canvas.drawPath( wallpath, mPaintBoundaryBoxZoneBorder );		
+		
+		// --- calc & draw nearest point
+        int radius = GenericOverlay.distanceInMap( centerLocation, 1 /* meters */, projection ); 		
+		projection.toPixels( z.nearestPoint, point );
+		canvas.drawCircle( point.x, point.y, radius, mPaintZoneCenterBorder );
+		
+		// --- calc & draw center point
+        int radiusCenter = GenericOverlay.distanceInMap( centerLocation, 1 /* meters */, projection ); 		
+		projection.toPixels( z.bbCenter, point );
+		canvas.drawCircle( point.x, point.y, radiusCenter, mPaintZoneCenterBorder );		
+		
+		// --- calc & draw pixel path of the zone
+
 		wallpath.reset(); // only needed when reusing this path for a new build
-		wallpath.moveTo( mPoints[0].x, mPoints[0].y); // used for first point
-		for( int i=1; i<number; i++ ) {
-			wallpath.lineTo(mPoints[i].x, mPoints[i].y);
+		projection.toPixels( z.points[0], point );
+		wallpath.moveTo( point.x, point.y );		
+		for( int i=1; i<z.points.length; i++ ) {
+			projection.toPixels( z.points[i], point );
+			wallpath.lineTo( point.x, point.y );
 		}
-		wallpath.lineTo(mPoints[0].x, mPoints[0].y);
+		wallpath.close();
+		canvas.drawPath( wallpath, mPaintVisibleZoneFill );		
+		canvas.drawPath( wallpath, mPaintVisibleZoneBorder );	
 		
 
-		canvas.drawPath(wallpath, mPaintVisibleZoneFill );		
-		canvas.drawPath(wallpath, mPaintVisibleZoneBorder );			
 	}
 	
     @Override
     public void drawBitmap(Canvas canvas, MapProjection projection) {
-
+    	Logger.v( TAG, "drawBitmap" );
+    	
     	if ( Engine.instance != null && Engine.instance.cartridge != null ) {
 			Vector<Zone> zones = Engine.instance.cartridge.zones;
 			for (int i = 0; i < zones.size(); i++) {
